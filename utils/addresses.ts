c0b1703fc07e193e55fd6c28c78900e02287aea6
Kyu <kyuhyun217@gmail.com>
2022-09-20 18:07:19 -0700

    Display EP address

---
+++ utils/addresses.ts
@@ -0,0 +1,24 @@
+import {
+  ChainId,
+  getContractAddressesForChainOrThrow,
+} from "@0x/contract-addresses";
+import { Chain } from "./constants";
+
+const CHAIN_TO_CHAIN_ID: Record<Chain, ChainId> = {
+  ethereum: ChainId.Mainnet,
+  polygon: ChainId.Polygon,
+  bsc: ChainId.BSC,
+  fantom: ChainId.Fantom,
+  avalanche: ChainId.Avalanche,
+  celo: ChainId.Celo,
+  optimism: ChainId.Optimism,
+  arbitrum: ChainId.Arbitrum,
+  ropsten: ChainId.Ropsten,
+  mumbai: ChainId.PolygonMumbai,
+  goerli: ChainId.Goerli,
+};
+
+export function getExchangeProxyAddress(chain: Chain): string {
+  return getContractAddressesForChainOrThrow(CHAIN_TO_CHAIN_ID[chain])
+    .exchangeProxy;
+}
